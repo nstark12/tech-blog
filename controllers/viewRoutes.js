d1970a12359808735bd96930d23edf0f41c19ef1
Marketing Director <nicolestark1103@gmail.com>
2023-09-06 10:21:51 -0500

    login route working

--- controllers/viewRoutes.js
+++ controllers/viewRoutes.js
@@ -14,4 +14,13 @@ router.get('/', async (req, res) => {
     }
 })
 
+// login page
+router.get('/login', async (req, res) => {
+    try {
+        res.render('login')
+    } catch(err) {
+        res.status(500).json(err)
+    }
+})
+
 module.exports = router
